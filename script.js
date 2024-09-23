b522836f2c7dfcfea4d5bb7528154a354ae550f6
Evan Barclay <119373672+BagpipesRbetter@users.noreply.github.com>
2024-09-22 20:08:56 -0600

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Ensure that the DOM content is fully loaded before attaching event listeners
 document.addEventListener('DOMContentLoaded', function() {
     var menuIcon = document.getElementById('menu-icon');
     var navLinks = document.getElementById('nav-links');
+    var arrow = document.getElementById('arrow');
 
     menuIcon.addEventListener('click', function() {
-        navLinks.classList.toggle('show');
+        navLinks.classList.toggle('show'); // Toggle menu visibility
     });
+
 });
